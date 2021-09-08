6a734eaf24bc6a833de6309e6877ca5464ecd38d
Rajesh Malviya <rajeshmalviya1710@gmail.com>
2021-09-08 23:08:37 +0530

    proxy added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App/App";
 import axios from "axios";
-axios.defaults.baseURL = "http://128.199.182.16:4000/";
+// axios.defaults.baseURL = "http://128.199.182.16:4000/";
 axios.defaults.headers.common["Authorization"] = localStorage.getItem("token");
 ReactDOM.render(
   <App />,
